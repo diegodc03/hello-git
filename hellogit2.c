a5c4a7783b0670e2f62375b409bacd3bd14c47eb
diegodc03 <diegodecm03@gmail.com>
2023-02-08 21:05:11 +0100

    Este es mi primer commit

---
+++ hellogit2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    printf("Hola Mundo");
+
+      printf("Hola Mundo");
+        printf("Hola Mundo");
+          printf("Hola Mundo");
+            printf("Hola Mundo");
+              printf("Hola Mundo");
+}
